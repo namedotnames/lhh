5c5ffd5245f3f4b1a97fc14a2d9de13f195c7f3d
namedotnames <51120972+namedotnames@users.noreply.github.com>
2019-10-13 22:49:07 -0400

    update

---
+++ src/scripts/veteran-suicides.js
@@ -0,0 +1,9 @@
+import * as d3 from 'd3'
+
+const margin = { top: 30, left: 25, right: 20, bottom: 20 }
+
+const height = 120 - margin.top - margin.bottom
+
+const width = 90 - margin.left - margin.right
+
+const container = d3.select('#veteran-suicides')
