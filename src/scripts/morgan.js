008af852032c0f78ec6f7f90ab3acd80ac107341
namedotnames <51120972+namedotnames@users.noreply.github.com>
2019-11-24 07:16:06 -0500

    update

---
+++ src/scripts/morgan.js
@@ -0,0 +1,39 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal("h1 span", {
+ delay: 200,
+ duration: 1000,
+ distance: "1000px"
+});
+
+
+ScrollReveal().reveal("h1 p", {
+ delay: 200,
+ duration: 1000,
+ origin: "right",
+ distance: "1000px"
+});
+
+ScrollReveal().reveal(".pullquote", {
+ delay: 200,
+ duration: 1000,
+ origin: "right",
+ distance: "1000px"
+});
+
+
+
+ScrollReveal().reveal("figure", {
+  delay: 200,
+ duration: 1000,
+ origin: "right",
+ distance: "1000px",
+});
+
+ScrollReveal().reveal("li", {
+  delay: 500,
+ duration: 1000,
+ origin: "bottom",
+ distance: "1000px"
+})
+
